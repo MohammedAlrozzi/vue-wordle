65b5457c38ba21dfefd891d57d7954b468c53e0b
Mohammed Alrozzi <alrozzi@protonmail.com>
2022-01-27 19:52:13 +0000

    first try

--- src/Keyboard.vue
+++ src/Keyboard.vue
@@ -10,9 +10,9 @@ defineEmits<{
 }>()
 
 const rows = [
-  'qwertyuiop'.split(''),
-  'asdfghjkl'.split(''),
-  ['Enter', ...'zxcvbnm'.split(''), 'Backspace']
+  'ضصثقفغعهخحجدذ'.split(''),
+  'شسيبلاأتنمكط'.split(''),
+  ['Enter', ...'ئءؤرىةوزظ'.split(''), 'Backspace']
 ]
 </script>
 
